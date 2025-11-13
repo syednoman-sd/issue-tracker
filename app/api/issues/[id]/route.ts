4baa90de0cf48e3fa810cff806f99f2af619e71d
Noman Syed <jdnomansyed@gmail.com>
2025-11-13 16:24:35 +0100

    build an API for deleting issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -7,7 +7,7 @@ export async function PATCH(
   { params }: { params: { id: string }}) {
   const { id } = await params;
   const body = await request.json();
-  
+
   const validation = issueSchema.safeParse(body);
   if(!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 })
@@ -24,4 +24,23 @@ export async function PATCH(
   })
 
   return NextResponse.json(updateIssue, { status: 201 }) 
+}
+
+export async function DELETE(
+  request: NextRequest, 
+  { params }: { params: { id: string }}) {
+  const { id } = await params;
+
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(id) }
+  })
+  
+  if(!issue)
+    return NextResponse.json({ error: 'Invalid issue' }, { status: 404 })
+
+  await prisma.issue.delete({
+    where: { id: parseInt(id) }
+  })  
+
+  return NextResponse.json({});
 }
