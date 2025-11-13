e320e00ee7ed6a577d17a7b1d2a3d2dbbb35f2e8
Noman Syed <jdnomansyed@gmail.com>
2025-11-13 07:34:42 +0100

    show issue details

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,5 +1,5 @@
 import { prisma } from '@/prisma/client'
-import { Button, Table } from '@radix-ui/themes'
+import { Table } from '@radix-ui/themes'
 import Link from 'next/link'
 import IssueStatusBadge from '../components/IssueStatusBadge'
 import delay from 'delay'
@@ -24,7 +24,9 @@ const IssuesPage = async () => {
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                {issue.title}
+                <Link href={`/issues/${issue.id}`}>
+                  {issue.title}
+                </Link>
                 <div className='block md:hidden'>
                   <IssueStatusBadge status={issue.status} />
                 </div>
